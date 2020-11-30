96f10b20854c01c123d6f50aa07bb32e1ffafbca
SimoniAccioly <67556668+SimoniAccioly@users.noreply.github.com>
2020-11-29 22:02:35 -0300

    alteracoes

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,9 +3,8 @@ import {Sequelize} from 'sequelize'
 
 //String de conexão - é uma string que tem todas as configurações para o sequelize conectar oao nosso banco de dados
 
-const pwd = encodeURIComponent('@teste@MySQL#')
 
-const sequelize = new Sequelize (`mysql://root:${pwd}@localhost:3306/pitu`)
+const sequelize = new Sequelize (`mysql://root:123#senha@localhost:3306/pitu`)
 //mysql://root(administrador):senhaMYSQL@localdoBD:Porta/nomedobd
 
 export default sequelize
